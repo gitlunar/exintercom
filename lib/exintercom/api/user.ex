4b89c15783975e55d759c586f0bff49fb202447f
Rohith Varanasi <rohvar98@gmail.com>
2016-04-06 11:07:22 -0400

    view user by user_id or email address

--- lib/exintercom/api/user.ex
+++ lib/exintercom/api/user.ex
@@ -5,14 +5,24 @@ defmodule ExIntercom.User do
 
   import ExIntercom.Base
 
-  @endpoint "https://api.intercom.io/users/"
+  @endpoint "https://api.intercom.io/users"
 
   @doc """
   Returns single user by its ID
   """
   @spec find([id: <<>>]) :: %{}
   def find(id: id) do
-    fetch(@endpoint <> to_string(id))
+    fetch(@endpoint <> "/" <> to_string(id))
+  end
+
+  @spec find_by_user_id([user_id: <<>>]) :: %{}
+  def find_by_user_id(user_id) do
+    fetch(@endpoint <> "?user_id=" <> user_id) 
+  end
+
+  @spec find_by_email([email: <<>>]) :: %{}
+  def find_by_email(email) do
+    fetch(@endpoint <> "?email=" <> email)
   end
 
   defp fetch(url) do
